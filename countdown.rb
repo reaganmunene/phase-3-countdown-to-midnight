71f410ee1dd1c90c8e9879afd28e4b68353fd9d4
reaganmunene <reagannmunene@gmail.com>
2023-07-07 12:25:16 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+require 'pry'
+def countdown(interger)
+  while interger > 0
+    puts "#{interger} SECOND(S)!"
+    sleep 5
+    interger -= 1
+    binding.pry
+  end
+ puts "HAPPY NEW YEAR!"
 
-def countdown
 end
+
+# countdown(10)
